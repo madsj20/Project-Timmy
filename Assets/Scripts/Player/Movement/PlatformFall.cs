23e2de00e5c9ec04c71614e79702851c77a431b9
mig <emzar21@student.sdu.dk>
2022-02-28 17:16:35 +0100

    movement fall through

---
+++ Assets/Scripts/Player/Movement/PlatformFall.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Collider2D m_ObjectCollider;
+    void Start()
+    {
+        m_ObjectCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform" && Input.GetKey("s"))
+        {
+            m_ObjectCollider.isTrigger = true;
+
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        m_ObjectCollider.isTrigger = false;
+    }
+}

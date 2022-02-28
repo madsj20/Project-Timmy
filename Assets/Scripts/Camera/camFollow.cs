5d889a4e65a170f46592a3521ee4630458cd3689
mig <emzar21@student.sdu.dk>
2022-02-28 15:53:35 +0100

    cam

--- Assets/Scripts/Camera/camFollow.cs
+++ Assets/Scripts/Camera/camFollow.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class camFollow : MonoBehaviour
 {
-    public Transform player;
+    private Transform player;
     // Start is called before the first frame update
-    void start()
+    void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }

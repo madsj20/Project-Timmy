390df17cfa96d2f9b0a17a45150339413113360e
mig <emzar21@student.sdu.dk>
2022-03-10 12:03:42 +0100

    bouncy stick

--- Assets/Scripts/Enviroment/BouncyStick.cs
+++ Assets/Scripts/Enviroment/BouncyStick.cs
@@ -4,13 +4,70 @@ using UnityEngine;
 
 public class BouncyStick : MonoBehaviour
 {
+    private float root;
+    private float timmyRoot;
+    private GameObject timmy;
+    private float distance;
+    private float branchWidth;
+
+    private float normDistance;
+    private float timmyVelocity;
     public float bounceForce = 20f;
+
+    private bool running;
+    public void Awake()
+    {
+        timmy = GameObject.FindWithTag("Player");
+        branchWidth = GetComponent<Collider2D>().bounds.size.x;
+
+    }
+    private void Start()
+    {
+        Debug.Log(root);
+        running = true;
+    }
+
+    private void Update()
+    {
+        
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+            
+            timmyRoot = timmy.transform.position.x;
+            root = this.gameObject.transform.GetChild(0).position.x;
+            distance = Mathf.Abs(timmyRoot - root);
+            //Mathf.InverseLerp(0, 1, distance);
+            normDistance = Mathf.InverseLerp(0, branchWidth, distance);
+            //collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * (normDistance * bounceForce * timmyVelocity), ForceMode2D.Impulse);
+            //StartCoroutine(GetVelocity());
+            if (running)
+            {
+                
+                StartCoroutine(JumpPause());
+            }
+            
+            //collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * (normDistance * bounceForce), ForceMode2D.Impulse);
+            Debug.Log(Mathf.InverseLerp(0, branchWidth, distance));
         }
     }
+   IEnumerator JumpPause()
+    {
+        running = false;
+        timmy.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * (normDistance * bounceForce), ForceMode2D.Impulse);
+        yield return new WaitForSeconds(0.5f);
+        running = true;
+    }
+
+    /*IEnumerator GetVelocity()
+    {
+
+        yield return new Wait
+        timmyVelocity = timmy.GetComponent<Rigidbody2D>().velocity.magnitude;
+    }*/
+
+
 }

f6c94c15d093690334e93a617ac9192cdfe993af
Mads Jensen <madsj20@student.sdu.dk>
2022-03-14 15:24:14 +0100

    Changed Jump particle effect to correct name

--- Assets/Scripts/Player/OutOfLevel.cs
+++ Assets/Scripts/Player/OutOfLevel.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class OutOfLevel : MonoBehaviour
 {
+    private Vector2 pos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pos = GetComponent<Transform>().position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (pos.x < -15)
+        {
+            GetComponent<Transform>().position = pos;
+            Debug.Log("Out of bounds");
+        }
     }
 }

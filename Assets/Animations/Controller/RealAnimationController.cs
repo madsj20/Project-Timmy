b7ae62a1fca53dda580edd55d1ab0a5c774d0347
Daniel Dalager <ddk.13@hotmail.com>
2022-03-23 12:32:24 +0100

    fixed animations (run works)

--- Assets/Animations/Controller/RealAnimationController.cs
+++ Assets/Animations/Controller/RealAnimationController.cs
@@ -5,28 +5,54 @@ using UnityEngine;
 public class RealAnimationController : MonoBehaviour
 {
     private Animator anim;
+
     void Awake()
     {
- 
         anim = GetComponent<Animator>();
     }
 
     public void Update()
     {
-        if (Input.GetKeyDown("w"))
+
+        if (Input.GetKeyDown("a") || Input.GetKeyDown("d")) // && (this.GetComponent<Movement>().isGrounded = true)) aka se om timmy står på jorden
         {
-            anim.SetBool("isJumping", true);
-        }
-        if (Input.GetKeyDown("a") || Input.GetKeyDown("d"))
-        {
-            //isRunnings = true;
             anim.SetBool("isRunnings", true);
         }
-        else
+        if (Input.GetKey("a") || Input.GetKey("d"))
         {
-
+            anim.SetBool("isRunnings", true);
+        }
+        if (Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+        {
             anim.SetBool("isRunnings", false);
         }
+
+        /*
+         if (Input.GetKeyDown("w"))
+         {
+             //stop();
+             anim.SetBool("isJumping", true);
+         }
+         if (Input.GetKeyDown("a") || Input.GetKeyDown("d"))
+         {
+             //stop();
+             anim.SetBool("isIdeal", false);
+             anim.SetBool("isRunnings", true);
+         }
+         else
+         {
+             anim.SetBool("isRunnings", false);
+         }
+        */
+    }
+    public void stop()
+    {
+        anim.SetBool("isRunnings", false);
+        anim.SetBool("isJumping", false);
+        anim.SetBool("isFalling", false);
+        anim.SetBool("isIdeal", false);
+        //anim.SetBool("isWall", false); //disse animationer er ikke lavet endnu 
+        //anim.SetBool("isSwinging", false); //same 
     }
 
 

a582479f3381b9ca60805d1d0100f90f01983270
g <emzar21@student.sdu.dk>
2022-04-03 19:32:41 +0200

    un-bugfixed knockback script
    gaming

--- Assets/Scripts/Player/Happiness.cs
+++ Assets/Scripts/Player/Happiness.cs
@@ -9,12 +9,19 @@ public class Happiness : MonoBehaviour
     public int currentHealth;
 
     public HappinessBar happinessBar;
+    public Transform enemyTf;
+    public bool isKnocked;
 
+    Rigidbody2D rb;
+    
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
         happinessBar.SetMaxHealth(maxHealth);
+
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
@@ -30,30 +37,43 @@ public class Happiness : MonoBehaviour
             TakeDamage(20);
         }
     }
+    private void FixedUpdate()
+    {
+        //rb.AddForce(Vector2.left * 20f);
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Bird")
+        
+        if (other.gameObject.tag == "Bird" && !isKnocked)
         {
+            enemyTf = other.transform;
             TakeDamage(10);
+            knockback();
         }
-        if (other.gameObject.tag == "Squirrel")
+        if (other.gameObject.tag == "Squirrel" && !isKnocked)
         {
+            enemyTf = other.transform;
             TakeDamage(10);
+            knockback();
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "BirdShit")
+        if (other.gameObject.tag == "BirdShit" && !isKnocked)
         {
+            enemyTf = other.transform;
             TakeDamage(15);
             Destroy(other.gameObject);
+            knockback();
         }
 
-        if (other.gameObject.tag == "Spikes")
+        if (other.gameObject.tag == "Spikes" && !isKnocked)
         {
+            enemyTf = other.transform;
             TakeDamage(15);
+            knockback();
             //add knockback
         }
     }
@@ -70,5 +90,30 @@ public class Happiness : MonoBehaviour
     {
         currentHealth += happiness;
         happinessBar.SetHealth(currentHealth);
+    }
+
+    void knockback()
+    {
+
+        StartCoroutine(CanMoveAgain());
+        rb.AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
+        if (transform.position.x < enemyTf.position.x)
+        {
+            rb.AddForce(Vector2.left * 10f, ForceMode2D.Impulse);
+        }
+        else
+        {
+            
+            rb.AddForce(Vector2.right * 10f, ForceMode2D.Impulse);
+        }
+        
+    }
+    IEnumerator CanMoveAgain()
+    {
+        isKnocked = true;
+        GetComponent<Movement>().canMove = false;
+        yield return new WaitForSeconds(1f);
+        GetComponent<Movement>().canMove = true;
+        isKnocked = false;
     }
 }

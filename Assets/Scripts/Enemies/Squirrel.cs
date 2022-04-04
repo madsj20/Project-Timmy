acbdf56b4a619cc93be49b738caa66a4529e87cf
Mads Jensen <madsj20@student.sdu.dk>
2022-04-04 11:43:33 +0200

    Squirrel changes direction when hitting player and wall

--- Assets/Scripts/Enemies/Squirrel.cs
+++ Assets/Scripts/Enemies/Squirrel.cs
@@ -8,21 +8,32 @@ public class Squirrel : Enemy
     public Transform groundCheck;
     public float checkRadius;
     public LayerMask whatIsGround;
+    private bool wallHit;
+    public Transform wallCheck;
+    public LayerMask whatIsWall;
     private bool flipPause = false;
 
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        wallHit = Physics2D.OverlapCircle(wallCheck.position, checkRadius, whatIsWall);
     }
 
     // Update is called once per frame
     protected override void Update()
     {
-        if (isGrounded == false && flipPause == false && canMove == true)
+        if ((isGrounded == false || wallHit == true) && flipPause == false && canMove == true)
         {
             StartCoroutine(FlipPause());
         }
         base.Update();
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(FlipPause());
+        }
     }
 
     IEnumerator FlipPause()

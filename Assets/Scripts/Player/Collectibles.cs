d422ef8aadeae3423c40788c05efcd629bc1e597
Mads Jensen <madsj20@student.sdu.dk>
2022-04-10 22:24:52 +0200

    Finished Collectibles speech bubble, and added heads with same size

--- Assets/Scripts/Player/Collectibles.cs
+++ Assets/Scripts/Player/Collectibles.cs
@@ -8,12 +8,17 @@ public class Collectibles : MonoBehaviour
     public GameObject collectedItem;
     public Sprite currentItem;
     public GameObject currentOwner;
+    private GameObject speachBubble;
 
     public Sprite jerome;
     public Sprite child1;
     public Sprite child2;
     public Sprite child3;
-    public Sprite Timmy;
+    public Sprite timmy;
+
+    private bool bubbleToPos = false;
+    public float speed = 8;
+
     //private string collect;
 
 
@@ -24,6 +29,7 @@ public class Collectibles : MonoBehaviour
     private void Start()
     {
         //Collect = GameObject.FindGameObjectsWithTag("Collectibles");
+        speachBubble = GameObject.Find("Collectibles Speach Bubble");
     }
 
     private void Update()
@@ -37,29 +43,37 @@ public class Collectibles : MonoBehaviour
                 currentOwner.GetComponent<Image>().sprite = child2;
                 break;
         }
-    }
 
+        //Lerp has to be in a loop to work, therefore i use at bool to activate it
+        if(bubbleToPos == true)
+        {
+            speachBubble.GetComponent<Transform>().position = Vector2.Lerp(speachBubble.GetComponent<Transform>().position, new Vector2(105f, 50), Time.deltaTime * speed);
+        }
+
+        if(bubbleToPos == false)
+        {
+            speachBubble.GetComponent<Transform>().position = Vector2.Lerp(speachBubble.GetComponent<Transform>().position, new Vector2(105f, -50), Time.deltaTime * speed);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
         if (collision.gameObject.CompareTag("Collectible"))
         {
             //CollectSound.Play();
+            StartCoroutine(Collectible());
             Debug.Log("collected");
             currentItem = collision.gameObject.GetComponent<SpriteRenderer>().sprite;
             collectedItem.GetComponent<Image>().sprite = currentItem;
-            StartCoroutine(Collectible());
             Destroy(collision.gameObject);
+            bubbleToPos = true;
         }
-
         
         IEnumerator Collectible()
         {
             yield return new WaitForSeconds(3f);
-            Debug.Log("collected3");
+            bubbleToPos = false;
 
-            
         }
     }
 

de2cc75863edf77077ab6c834c2ff22651f8aa0e
Mads Jensen <madsj20@student.sdu.dk>
2022-04-30 09:24:59 +0200

    Added escape button to reset level

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -16,6 +17,9 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("PlayScene");
+        }
     }
 }

ff9bddc206e5c95e1fb6e4a2a401df91f7cfe405
Daniel Dalager <ddk.13@hotmail.com>
2022-05-05 11:35:18 +0200

    Mere lyd stuff

--- Assets/Scripts/Audio controller/AudioController.cs
+++ Assets/Scripts/Audio controller/AudioController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
+    public static AudioClip UIClick, Crying, Jump, Landing, Hit;
+    static AudioSource Audiosrc;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UIClick = Resources.Load<AudioClip>("click5");
+        Crying = Resources.Load<AudioClip>("Crying-Timmy");
+        Jump = Resources.Load<AudioClip>("Crying-Timmy");
+        Landing = Resources.Load<AudioClip>("Landing-Timmy");
+        Hit = Resources.Load<AudioClip>("Lossing-HP-Timmy");
+
+        Audiosrc = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public static void playSound(string clip) 
+    {
+        switch (clip)
+        {
+            case "UIClick":
+                Audiosrc.PlayOneShot(UIClick);
+            break;
+
+            case "Crying":
+                Audiosrc.PlayOneShot(Crying);
+                break;
+
+            case "Jump":
+                Audiosrc.PlayOneShot(Jump);
+                break;
+
+            case "Landing":
+                Audiosrc.PlayOneShot(Landing);
+                break;
+
+            case "Hit":
+                Audiosrc.PlayOneShot(Hit);
+                break;
+        }
     }
 }

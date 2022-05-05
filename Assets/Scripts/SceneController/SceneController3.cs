4379ef41cf38b80986eb88da3ed98c3e14f0850c
Daniel Dalager <ddk.13@hotmail.com>
2022-05-05 10:03:28 +0200

    færdig med cut sences

--- Assets/Scripts/SceneController/SceneController3.cs
+++ Assets/Scripts/SceneController/SceneController3.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneController3 : MonoBehaviour
+public class SceneController2 : MonoBehaviour
 {
     public float timerToNextScene1;
     // Start is called before the first frame update
     public void Start()
     {
-        timerToNextScene1 = 15;
+        timerToNextScene1 = 21;
         Invoke("Play", timerToNextScene1);
     }
 
     void Play()
     {
-        SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene("ending");
     }
 
 

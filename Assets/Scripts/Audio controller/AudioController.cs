ea46f2789dc0a42bca1bde6919a7fd8a7ab874a6
Daniel Dalager <ddk.13@hotmail.com>
2022-05-09 15:12:34 +0200

    Lyd

--- Assets/Scripts/Audio controller/AudioController.cs
+++ Assets/Scripts/Audio controller/AudioController.cs
@@ -11,7 +11,6 @@ public class AudioController : MonoBehaviour
 
     private void Awake()
     {
-
         Audiosrc = this.GetComponent<AudioSource>();
         Instance = this;
 
@@ -26,45 +25,9 @@ public class AudioController : MonoBehaviour
         }
     }
 
-
-    // Start is called before the first frame update
-    void Start()
+    public static void playSound() 
     {
-
+        AudioClip[] audioClips;
+        
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    //public static void playSound(string clip) 
-    //{
-    //    switch (clip)
-    //    {
-    //        case "UIClick":
-    //            Audiosrc.PlayOneShot(UIClick);
-    //        break;
-    //
-    //        case "Crying":
-    //            Audiosrc.PlayOneShot(Crying);
-    //            break;
-    //
-    //        case "Jump":
-    //            Audiosrc.PlayOneShot(Jump);
-    //            break;
-    //
-    //        case "Landing":
-    //            Audiosrc.PlayOneShot(Landing);
-    //            break;
-    //
-    //        case "Hit":
-    //            Audiosrc.PlayOneShot(Hit);
-    //            Debug.Log("played hit sound");
-    //            break;
-    //        default:
-    //            Debug.Log("Something failed, propobely Mads, just blame him <3");
-    //            break;
-    //    }
-    //}
 }

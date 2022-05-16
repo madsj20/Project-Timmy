0397fd3db45e1427a6ecbba997c186826a0bd6d6
mig <emzar21@student.sdu.dk>
2022-05-16 11:51:28 +0200

    gaming

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class SceneManagement : MonoBehaviour
 {
 
+    public AudioSource audioSource;
+    public AudioClip clickUI;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        clickUI = (AudioClip)Resources.Load("Audio/Sounds/UI/click5");
+    }
+
     public void SceneLoader()
     {
+        audioSource.PlayOneShot(clickUI, 1f);
         SceneManager.LoadScene("Classroom");
     }
 
     public void Exit()
     {
+        audioSource.PlayOneShot(clickUI, 1f);
         Application.Quit();
     }
 }
